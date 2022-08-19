079c7cc599333c4b7ed1fc95403afa86efc228a7
isaacbrds <isaacbrds>
2022-08-19 16:20:19 -0300

    Introduçao a Javascript - Exercicios Todo List

--- aula-js-xp/exercicios.js
+++ aula-js-xp/exercicios.js
@@ -3,3 +3,17 @@ function convertToFarenheit(value){
 }
 
 console.log(`O valor em farenheit é: ${convertToFarenheit(100)}`);
+
+
+const entrada =  document.getElementById('input-todo')
+const botao = document.getElementById('btn-adiciona')
+let lista = document.getElementById('lista-tarefas');
+
+botao.addEventListener('click', function(evento){
+    evento.preventDefault();
+    const item = document.createElement('li')
+    item.innerText = entrada.value
+    lista.appendChild(item)
+    entrada.value = ''
+})
+
