a630b4c2a170e8b0b31bf57346da6dbfe522eedb
isaacbrds <isaac.brigido@gmail.com>
2022-08-19 10:14:06 -0300

    Introdução ao JS - Operadores

---
+++ aula-js-xp/script.js
@@ -0,0 +1,14 @@
+let sum = 2 + 2
+console.log('sum: ' + sum)
+
+let sub = 3 - 3
+console.log('sub: ' + sub)
+
+let mul = 3 * 3
+console.log('mul: ' + mul)
+
+let div = 3 / 2
+console.log('div: ' + div)
+
+let rest = 3 % 2
+console.log('rest: ' + rest)
