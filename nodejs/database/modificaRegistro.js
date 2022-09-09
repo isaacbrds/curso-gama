ac130583519d2dde059606ab9e4c688114b48634
isaacbrds <isaacbrds>
2022-09-09 19:20:56 -0300

    Nodejs - alterando registro no banco de dados

---
+++ nodejs/database/modificaRegistro.js
@@ -0,0 +1,23 @@
+let mysql = require('mysql2');
+
+
+let conexao = mysql.createConnection({
+    host: "localhost",
+    user: "isaac",
+    password: "123",
+    database: 'nodejs'
+});
+
+conexao.connect(function(err){
+    if(err) throw err;
+
+    console.log('conectado!');
+    
+    let sql = "update consumidores set nome_consumidor = 'Ana JS' where(nome_consumidor='Ana')"
+
+    conexao.query(sql, function(err, result){
+        if(err) throw err;
+        console.log(result);
+    })
+
+});
